72a39ba1e4d39ff30c29ec9589e038fb3d109711
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-03-17 14:38:58 +0600

    first commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,44 @@
-import React, { useContext } from 'react';
-
+// import firebase from 'firebase';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import React, { useContext } from "react";
+import { UserContext } from "../../App";
+import firebaseConfig from "./firebase.config";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
-    );
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  const handleGoogleSignIn = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        const { displayName, email } = result.user;
+        const signedInUser = { name: displayName, email };
+        setLoggedInUser(signedInUser);
+        console.log(signedInUser);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  };
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCig_V2g2w0A8fbP62bZj8fzqDQIaw9g2M",
+    authDomain: "burj-al-arab-19c6c.firebaseapp.com",
+    projectId: "burj-al-arab-19c6c",
+    storageBucket: "burj-al-arab-19c6c.appspot.com",
+    messagingSenderId: "73554105793",
+    appId: "1:73554105793:web:e80fb7979b7a7b392e0b40"
+  };
+
+  export default firebaseConfig;
