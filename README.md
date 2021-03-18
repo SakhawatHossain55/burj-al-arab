b1768662654a51dc8f7dc2163a6b9f0d805722b7
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-03-18 18:18:27 +0600

    first commit

--- README.md
+++ README.md
@@ -74,3 +74,4 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/tr
 # burj-al-arab
 # burj-ar-arab
 # burj-al-arab
+# burj-al-arab
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,11 +2,16 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import React, { useContext } from "react";
+import { useHistory, useLocation } from "react-router";
 import { UserContext } from "../../App";
 import firebaseConfig from "./firebase.config";
 
 const Login = () => {
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: "/" } };
+
   if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
   }
@@ -20,17 +25,12 @@ const Login = () => {
         const { displayName, email } = result.user;
         const signedInUser = { name: displayName, email };
         setLoggedInUser(signedInUser);
-        console.log(signedInUser);
+        history.replace(from);
+        // console.log(signedInUser);
       })
       .catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+        const errorMessage = error.message;
+        console.log(errorMessage);
       });
   };
   return (
